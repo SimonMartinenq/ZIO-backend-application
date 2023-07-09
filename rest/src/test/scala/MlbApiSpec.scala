9562c3a240819774ecc0547f3b3a404bfb15d84f
AstrydCasimir <astryd.casimirgressier@gmail.com>
2023-07-09 17:07:39 +0200

    Update MlbApiSpec.scala

--- rest/src/test/scala/MlbApiSpec.scala
+++ rest/src/test/scala/MlbApiSpec.scala
@@ -5,9 +5,15 @@ class MlbApiSpec extends munit.ZSuite {
 
   val app: App[Any] = MlbApi.static
 
-  testZ("should be ok") {
+  testZ("should be ok text") {
 
     val req = Request.get(URL(Root / "text"))
+    assertZ(app.runZIO(req).isSuccess)
+  }
+
+  testZ("should be ok json") {
+
+    val req = Request.get(URL(Root / "json"))
     assertZ(app.runZIO(req).isSuccess)
   }
 
